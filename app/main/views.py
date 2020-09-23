1aea52361974c648519a8347c4cc49adc5c9ffbd
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-23 16:02:52 +0300

    add a route to the views file for filtering by categories

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,8 @@ main = Blueprint('main', __name__)
 def home():
   posts = Post.query.order_by(Post.date_posted.desc()).all()
   return render_template('index.html', posts = posts)
+
+@main.route('/category/<category>')
+def post_by_category(category):
+  posts = Post.query.filter_by(category=category).order_by(Post.date_posted.desc()).all()
+  return render_template('post_by_category.html', posts = posts)
