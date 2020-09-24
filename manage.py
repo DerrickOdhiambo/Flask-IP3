454bce9521eced7f5acf094f626b578659faa479
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-24 08:57:02 +0300

    add the manager command decorator for testing

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User,Post
+from app.models import User,Post, Comments
 
 #creating app instance
 app = create_app('development')
@@ -10,13 +10,21 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-  return dict(app = app, db = db, User = User, Post = Post)
+  return dict(app = app, db = db, User = User, Post = Post, Comments = Comments)
 
 if __name__=='__main__':
   manager.run()
