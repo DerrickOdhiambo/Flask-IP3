fabda84657aada8c0e3e352f81f93414adfbd6a8
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-24 08:07:49 +0300

    change class name to suit a particular style

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:derrick@localhost/pitch'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
