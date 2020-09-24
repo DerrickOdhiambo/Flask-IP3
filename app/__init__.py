e6281d66e99b1ae3b27984174b7fd240aa3f069e
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-24 08:48:53 +0300

    create new init structure for the configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,43 @@
+import os
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from config import config_options
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '973f7325c8e5e5d527c63ed6051365'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from app import views
+
+def create_app(config_name):
+  app = Flask(__name__)
+
+  # creating app configurations
+  app.config.from_object(config_options[config_name])
+
+  #initializing extensions
+  login_manager.init_app(app)
+  mail.init_app(app)
+  db.init_app(app)
+  bcrypt.init_app(app)
+
+
+  #registering routes
+  from app.main.views import main
+  from app.users.views import users
+  from app.posts.views import posts
+
+  #registering the blueprints
+  app.register_blueprint(users)
+  app.register_blueprint(posts)
+  app.register_blueprint(main)
+
+
+  return app
