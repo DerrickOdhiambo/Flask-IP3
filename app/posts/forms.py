29c2b2df896820b023fd714f5efa308482c6ae04
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-24 08:47:51 +0300

    add a select field to the post form for the dropdown

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    category = StringField('Title', validators=[DataRequired()])
+    category = SelectField(u'Categories', choices=[('Career', 'Career'), ('PickUp_Lines', 'PickUp_Lines'), ('Slogans', 'Slogans'), ('Product', 'Product')])
     content = TextAreaField('Pitch', validators=[DataRequired()])
     submit = SubmitField('Post')
 
@@ -11,3 +11,5 @@ class PostForm(FlaskForm):
 class CommentForm(FlaskForm):
     text = TextAreaField('Comment')
     submit = SubmitField('Post')
+
+
